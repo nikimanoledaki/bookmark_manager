b0ac9e419d6772942882d2f2569c9d5f960a4be4
niki-01 <niki.manoledaki@gmail.com>
2020-01-16 11:29:51 +0000

    Creates feature to add bookmark

--- app.rb
+++ app.rb
@@ -2,6 +2,19 @@ require 'sinatra/base'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+  get '/' do
+    "Hello world!"
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+  post '/bookmarks' do
+    Bookmark.create(url: params['url'])
+    redirect '/bookmarks'
+  end
+
   get '/bookmarks' do
     @bookmark = Bookmark.all
     erb :bookmarks
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,12 +1,10 @@
 require 'pg'
 
 feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+  scenario 'visiting /bookmarks shows me all the bookmarks' do
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.destroyallsoftware.com")
+    Bookmark.create(url: "http://www.google.com")
 
     visit('/bookmarks')
 
