511665a389a99895ac70d6743827bc8674760b2a
niki-01 <niki.manoledaki@gmail.com>
2020-01-17 17:21:13 +0000

    Refactors code to have new methods .find and .update

--- app.rb
+++ app.rb
@@ -18,19 +18,17 @@ class BookmarkManager < Sinatra::Base
   end
 
   delete '/bookmarks/:id' do
-    p params
     Bookmark.delete(id: params[:id])
     redirect '/bookmarks'
   end
 
   get '/bookmarks/:id/edit' do
-    @bookmark_id = params[:id]
-    erb :'bookmarks/edit'
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :"bookmarks/edit"
   end
 
   patch '/bookmarks/:id' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
     redirect '/bookmarks'
   end
 
